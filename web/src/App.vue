c16c9546c06f0eae73f63e28f1750e37cf1089f5
Tercio de Melo <terciodemelo@gmail.com>
2019-02-22 14:12:16 -0300

    Change initial page text for VTEX branding

--- web/src/App.vue
+++ web/src/App.vue
@@ -12,7 +12,7 @@
           <md-list v-if="$auth.loggedIn" class="md-transparent">
             <md-list-item class="md-avatar-list">
               <md-avatar class="md-large">
-                <img v-bind:src="$auth.userInfo.imageUri" alt="People"/>
+                <img v-bind:src="$auth.userInfo.imageUri" alt="People">
               </md-avatar>
             </md-list-item>
             <md-list-item>
@@ -27,7 +27,10 @@
           <md-list-item v-if="!$auth.loggedIn" @click="routeTo('/sign-in')">
             <md-icon>
               <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                <path fill="#000000" d="M19,3H5C3.89,3 3,3.89 3,5V9H5V5H19V19H5V15H3V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M10.08,15.58L11.5,17L16.5,12L11.5,7L10.08,8.41L12.67,11H3V13H12.67L10.08,15.58Z" />
+                <path
+                  fill="#000000"
+                  d="M19,3H5C3.89,3 3,3.89 3,5V9H5V5H19V19H5V15H3V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M10.08,15.58L11.5,17L16.5,12L11.5,7L10.08,8.41L12.67,11H3V13H12.67L10.08,15.58Z"
+                ></path>
               </svg>
             </md-icon>
             <span class="md-list-item-text">Sign In</span>
@@ -47,7 +50,10 @@
           <md-list-item v-if="$auth.loggedIn" @click="signOut()">
             <md-icon>
               <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                <path fill="#000000" d="M14.08,15.59L16.67,13H7V11H16.67L14.08,8.41L15.5,7L20.5,12L15.5,17L14.08,15.59M19,3A2,2 0 0,1 21,5V9.67L19,7.67V5H5V19H19V16.33L21,14.33V19A2,2 0 0,1 19,21H5C3.89,21 3,20.1 3,19V5C3,3.89 3.89,3 5,3H19Z" />
+                <path
+                  fill="#000000"
+                  d="M14.08,15.59L16.67,13H7V11H16.67L14.08,8.41L15.5,7L20.5,12L15.5,17L14.08,15.59M19,3A2,2 0 0,1 21,5V9.67L19,7.67V5H5V19H19V16.33L21,14.33V19A2,2 0 0,1 19,21H5C3.89,21 3,20.1 3,19V5C3,3.89 3.89,3 5,3H19Z"
+                ></path>
               </svg>
             </md-icon>
             <span class="md-list-item-text">Sign Out</span>
@@ -66,34 +72,39 @@ export default {
   name: 'app',
   data: () => ({
     menuVisible: false,
-    titleMsg: 'Account Manager'
+    titleMsg: 'Welcome to VTEX Wi-Fi! Sign below with your @vtex.com.br'
   }),
   methods: {
     open() {
-      console.log('Opened menu')
+      console.log('Opened menu');
     },
     close() {
-      console.log('Closed menu')
+      console.log('Closed menu');
     },
     routeTo(ref) {
-      console.info('Routing to ' + ref)
-      this.$router.push(ref)
-      this.menuVisible = false
+      console.info('Routing to ' + ref);
+      this.$router.push(ref);
+      this.menuVisible = false;
     },
     signOut() {
-      console.info('Signing out...')
-      this.menuVisible = false
-      this.$auth.logout()
+      console.info('Signing out...');
+      this.menuVisible = false;
+      this.$auth.logout();
     },
     isAdmin() {
-      return this.$auth.loggedIn && this.$auth.userInfo.amAdmin
+      return this.$auth.loggedIn && this.$auth.userInfo.amAdmin;
     }
   }
-}
+};
 </script>
 
 <style>
 .md-app {
   min-height: 100vh;
+}
+
+.md-primary {
+  background-color: #f71963 !important;
+  background-color: var(--md-theme-default-primary, #f71963);
 }
 </style>
